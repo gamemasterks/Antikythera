01a555e961b15ea671c84baa6a927e8869b7835d
gamemasterks <69360468+gamemasterks@users.noreply.github.com>
2020-10-17 11:23:13 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,97 @@
 ﻿using System;
 using System.Threading;
 using Microsoft.SPOT;
-using System.Text;
 using Microsoft.SPOT.Hardware;
+using System.Text;
 using CTRE.Phoenix;
 using CTRE.Phoenix.MotorControl;
 using CTRE.Phoenix.MotorControl.CAN;
 using CTRE.Phoenix.Controller;​
 namespace AntikytheraRobotCode
 {
-    public enum FlagState
+    public class Program
+    {
+        /* create a talon */
+        static TalonSRX rightSlave = new TalonSRX(4);
+        static TalonSRX right = new TalonSRX(3);
+        static TalonSRX leftSlave = new TalonSRX(2);
+        static TalonSRX left = new TalonSRX(1);
+
+        static StringBuilder stringBuilder = new StringBuilder();
+
+        static CTRE.Phoenix.Controller.GameController _gamepad = new GameController(UsbHostDevice.GetInstance());
+
+        public static void Main()
+        {
+            /* Factory Default all hardware to prevent unexpected behaviour */
+            rightSlave.ConfigFactoryDefault();
+            right.ConfigFactoryDefault();
+            leftSlave.ConfigFactoryDefault();
+            left.ConfigFactoryDefault();
+            /* loop forever */
+            while (true)
+            {
+                /* drive robot using gamepad */
+                Drive();
+                /* print whatever is in our string builder */
+                Debug.Print(stringBuilder.ToString());
+                stringBuilder.Clear();
+                /* feed watchdog to keep Talon's enabled */
+                CTRE.Phoenix.Watchdog.Feed();
+                /* run this task every 20ms */
+                Thread.Sleep(20);
+            }
+        }
+        /**
+         * If value is within 10% of center, clear it.
+         * @param value [out] floating point value to deadband.
+         */
+        static void Deadband(ref float value)
+        {
+            if (value < -0.10)
+            {
+                /* outside of deadband */
+            }
+            else if (value > +0.10)
+            {
+                /* outside of deadband */
+            }
+            else
+            {
+                /* within 10% so zero it */
+                value = 0;
+            }
+        }
+        static void Drive()
+        {
+            float x = _gamepad.GetAxis(0);
+            float y = -1 * _gamepad.GetAxis(1);
+            float twist = _gamepad.GetAxis(2);
+
+            Deadband(ref x);
+            Deadband(ref y);
+            Deadband(ref twist);
+
+            float leftThrot = y + twist;
+            float rightThrot = y - twist;
+
+            left.Set(ControlMode.PercentOutput, leftThrot);
+            leftSlave.Set(ControlMode.PercentOutput, leftThrot);
+            right.Set(ControlMode.PercentOutput, -rightThrot);
+            rightSlave.Set(ControlMode.PercentOutput, -rightThrot);
+
+            stringBuilder.Append("\t");
+            stringBuilder.Append(x);
+            stringBuilder.Append("\t");
+            stringBuilder.Append(y);
+            stringBuilder.Append("\t");
+            stringBuilder.Append(twist);
+
+        }
+    }
+}
+
+public enum FlagState
     {
         TURN,
         TURN_SLOW,
@@ -18,93 +100,12 @@ namespace AntikytheraRobotCode
         INIT,
         UNKNOWN
     }​
-   public class Program
+   public class Flag
     {​
-         public class Program
-        {
-            /* create a talon */
-            static TalonSRX rightSlave = new TalonSRX(4);
-            static TalonSRX right = new TalonSRX(3);
-            static TalonSRX leftSlave = new TalonSRX(2);
-            static TalonSRX left = new TalonSRX(1);
-
-            static StringBuilder stringBuilder = new StringBuilder();
-
-            static CTRE.Phoenix.Controller.GameController _gamepad = new GameController(UsbHostDevice.GetInstance());
-
-            public static void Main()
-            {
-                /* Factory Default all hardware to prevent unexpected behaviour */
-                rightSlave.ConfigFactoryDefault();
-                right.ConfigFactoryDefault();
-                leftSlave.ConfigFactoryDefault();
-                left.ConfigFactoryDefault();
-                /* loop forever */
-                while (true)
-                {
-                    /* drive robot using gamepad */
-                    Drive();
-                    /* print whatever is in our string builder */
-                    Debug.Print(stringBuilder.ToString());
-                    stringBuilder.Clear();
-                    /* feed watchdog to keep Talon's enabled */
-                    CTRE.Phoenix.Watchdog.Feed();
-                    /* run this task every 20ms */
-                    Thread.Sleep(20);
-                }
-            }
-            /**
-             * If value is within 10% of center, clear it.
-             * @param value [out] floating point value to deadband.
-             */
-            static void Deadband(ref float value)
-            {
-                if (value < -0.10)
-                {
-                    /* outside of deadband */
-                }
-                else if (value > +0.10)
-                {
-                    /* outside of deadband */
-                }
-                else
-                {
-                    /* within 10% so zero it */
-                    value = 0;
-                }
-            }
-            static void Drive()
-            {
-                float x = _gamepad.GetAxis(0);
-                float y = -1 * _gamepad.GetAxis(1);
-                float twist = _gamepad.GetAxis(2);
-
-                Deadband(ref x);
-                Deadband(ref y);
-                Deadband(ref twist);
-
-                float leftThrot = y + twist;
-                float rightThrot = y - twist;
-
-                left.Set(ControlMode.PercentOutput, leftThrot);
-                leftSlave.Set(ControlMode.PercentOutput, leftThrot);
-                right.Set(ControlMode.PercentOutput, -rightThrot);
-                rightSlave.Set(ControlMode.PercentOutput, -rightThrot);
-
-                stringBuilder.Append("\t");
-                stringBuilder.Append(x);
-                stringBuilder.Append("\t");
-                stringBuilder.Append(y);
-                stringBuilder.Append("\t");
-                stringBuilder.Append(twist);
-
-            }
-        }
-    }
-    private static TalonSRX talon = new TalonSRX(0);
-       private static TalonSRX talon2 = new TalonSRX(1);​
+       private static TalonSRX talon = new TalonSRX(0);
+        private static TalonSRX talon2 = new TalonSRX(1);​
        private static FlagState currentFlagState = FlagState.INIT;
-       private static FlagState lastFlagState = FlagState.UNKNOWN;​
+        private static FlagState lastFlagState = FlagState.UNKNOWN;​
        private static GameController gamepad = new GameController(new CTRE.Phoenix.UsbHostDevice(0));​
 ​
        public static void Main()
@@ -128,7 +129,7 @@ namespace AntikytheraRobotCode
                     else
                     {
                         on = true; // set to on
-                        talon.Set(ControlMode.PercentOutput, 0.5);  // set talon 50%
+                        talon.Set(ControlMode.PercentOutput, 1);  // set talon 100%
                     }
                 }​
                if (gamepad.GetButton(0)) // is button with id 0 pressed
@@ -150,7 +151,7 @@ namespace AntikytheraRobotCode
 ​
                if (gamepad.GetButton(4))
                 {
-                    talon2.Set(ControlMode.PercentOutput, 0.5); // set talon to 50%
+                    talon2.Set(ControlMode.PercentOutput, 1.0); // set talon to 100%
                 }
                 else
                 {
@@ -161,9 +162,9 @@ namespace AntikytheraRobotCode
 ​
                if (currentFlagState != lastFlagState) // make code not run every update to have faster run time
                 {
-                lastFlagState = currentFlagState; // update the last state for flag spinner
-                switch (currentFlagState) // switch on current state for flag spinner
-                {
+                    lastFlagState = currentFlagState; // update the last state for flag spinner
+                    switch (currentFlagState) // switch on current state for flag spinner
+                    {
                         case FlagState.INIT: // is it in INIT?
                             talon.Set(ControlMode.PercentOutput, 0.0); // set talon to 0%
                             currentFlagState = FlagState.IDLE; // set current state to IDLE
@@ -172,7 +173,7 @@ namespace AntikytheraRobotCode
                             talon.Set(ControlMode.PercentOutput, 0.0); // set talon to 0%
                             break;
                         case FlagState.TURN:
-                            talon.Set(ControlMode.PercentOutput, 0.5);  // set talon 50%
+                            talon.Set(ControlMode.PercentOutput, 1);  // set talon 100%
                             break;
                         case FlagState.TURN_SLOW:
                             talon.Set(ControlMode.PercentOutput, 0.2);  // set talon 20%
